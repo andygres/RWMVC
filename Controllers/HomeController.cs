d48bd3570e94a6f7de7865490af73d589a7bdec8
andyg <andyg@morrealeres.com>
2021-04-09 12:52:46 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RWMVC.Models;
+
+namespace RWMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SecureUpload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DoUpload(Document doc)
+        {
+            ViewBag.thing = doc.FileNumber;
+
+            return View();
+
+
+            //if (doc.HasFile && fileNumValid)
+            //{
+            //    try
+            //    {
+            //        uploadDoc(lblFileID.Text, fuDocument.FileBytes, fuDocument.FileName.ToString(), Convert.ToInt32(ddlDocType.SelectedValue), txtDescription.Text);
+            //        uploadSuccess = true;
+            //    }
+            //    catch (AggregateException)
+            //    {
+            //        lblComplete.Text = "Something went wrong. Please try again later.";
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File number" + lblFileID + " invalid");
+            //}
+        }
+    }
+}
---
+++ Models/Document.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RWMVC.Models
+{
+    public class Document
+    {
+        [Required]
+        public string FileNumber { get; set; }
+        [Required]
+        public byte[] File { get; set; }
+        public string DocumentType { get; set; }
+        public string Description { get; set; }
+    }
+
+    
+}
